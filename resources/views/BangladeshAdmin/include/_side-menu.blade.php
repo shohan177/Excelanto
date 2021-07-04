baaf467e9e77affbaea7b26d74ec8dbdc6060964
Arafat7616 <arafathossin7616@gmail.com>
2021-07-04 10:52:40 +0600

    Update sitde-menu missing changed

--- resources/views/BangladeshAdmin/include/_side-menu.blade.php
+++ resources/views/BangladeshAdmin/include/_side-menu.blade.php
@@ -18,27 +18,27 @@
                 <li class="has_sub">
                     <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-users"></i> <span> One Stop Service </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
                     <ul class="list-unstyled">
-                        <li><a href="ui-buttons.html"><i class="fa fa-arrow-circle-right"></i> New Requests</a></li>
-                        <li><a href="ui-panels.html"><i class="fa fa-arrow-circle-right"></i> Approved Requests</a></li>
-                        <li><a href="ui-tabs-accordions.html"><i class="fa fa-arrow-circle-right"></i> Rejected Requests</a></li>
+                        <li><a href="{{ route('oss_company_request') }}"><i class="fa fa-arrow-circle-right"></i> New Requests</a></li>
+                        <li><a href="{{ route('oss_approved_request') }}"><i class="fa fa-arrow-circle-right"></i> Approved Requests</a></li>
+                        <li><a href="{{ route('oss_rejected_request') }}"><i class="fa fa-arrow-circle-right"></i> Rejected Requests</a></li>
                     </ul>
                 </li>
 
                 <li class="has_sub">
                     <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-users"></i> <span> Welfare Service (UAE) </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
                     <ul class="list-unstyled">
-                        <li><a href="ui-buttons.html"><i class="fa fa-arrow-circle-right"></i> New Requests</a></li>
-                        <li><a href="ui-panels.html"><i class="fa fa-arrow-circle-right"></i> Approved Requests</a></li>
-                        <li><a href="ui-tabs-accordions.html"><i class="fa fa-arrow-circle-right"></i> Rejected Requests</a></li>
+                        <li><a href="{{ route('welfare_company_request') }}"><i class="fa fa-arrow-circle-right"></i> New Requests</a></li>
+                        <li><a href="{{ route('welfare_approved_request') }}"><i class="fa fa-arrow-circle-right"></i> Approved Requests</a></li>
+                        <li><a href="{{ route('welfare_rejected_request') }}"><i class="fa fa-arrow-circle-right"></i> Rejected Requests</a></li>
                     </ul>
                 </li>
 
                 <li class="has_sub">
                     <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-users"></i> <span> Bangladesh Embassy </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
                     <ul class="list-unstyled">
-                        <li><a href="ui-buttons.html"><i class="fa fa-arrow-circle-right"></i> New Requests</a></li>
-                        <li><a href="ui-panels.html"><i class="fa fa-arrow-circle-right"></i> Approved Requests</a></li>
-                        <li><a href="ui-tabs-accordions.html"><i class="fa fa-arrow-circle-right"></i> Rejected Requests</a></li>
+                        <li><a href="{{ route('bangladesh_embassy_request') }}"><i class="fa fa-arrow-circle-right"></i> New Requests</a></li>
+                        <li><a href="{{ route('bangladesh_embassy_approved_request') }}"><i class="fa fa-arrow-circle-right"></i> Approved Requests</a></li>
+                        <li><a href="{{ route('bangladesh_embassy_rejected_request') }}"><i class="fa fa-arrow-circle-right"></i> Rejected Requests</a></li>
                     </ul>
                 </li>
 
