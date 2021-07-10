3417db0654b12d67cd7a8a9ca7ad2e04b6013621
jahid hassan shovon <jahidhassandiu91@gmail.com>
2021-07-10 14:32:00 +0600

    sidebar modify

--- resources/views/SuperAdmin/include/_side-menu.blade.php
+++ resources/views/SuperAdmin/include/_side-menu.blade.php
@@ -7,9 +7,9 @@
                     <a href="{{ route('SuperAdmin.dashboard') }}" class="waves-effect"><i class="ti-home"></i><span> Dashboard </span></a>
                 </li>
                 <li class="has_sub">
-                    <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-account-key"></i> <span> Role's </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
+                    <a {{Request::is('SuperAdmin.role.*') ? 'active': ''}} href="{{ route('SuperAdmin.role.index') }}" class="waves-effect"><i class="mdi mdi-account-key"></i> <span> Role's </span> <span class="pull-right"><i class="mdi mdi-plus"></i></span></a>
                     <ul class="list-unstyled">
-                        <li><a href="{{ route('SuperAdmin.role.index') }}"><i class="fa fa-arrow-circle-right"></i> All Role</a></li>
+                        <li><a href=""><i class="fa fa-arrow-circle-right"></i> All Role</a></li>
                     </ul>
                 </li>
 
