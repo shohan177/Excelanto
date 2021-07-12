7f7f38e9e5899b90904ae215f2a586539d3a5547
jahid hassan shovon <jahid15-1499@diu.edu.bd>
2021-07-12 15:42:27 +0600

    Login Done & Update Database

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,56 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo;
+
+    public function redirectTo()
+    {
+        if (Auth::user()->user_type == 'super-admin'){
+            return redirect()->route('SuperAdmin.dashboard');
+        }elseif(Auth::user()->user_type == 'employer-company')
+        {
+            return redirect()->route('EmployerCompany.dashboard');
+        }elseif(Auth::user()->user_type == 'welfare-service-center-company')
+        {
+            return redirect()->route('WelfareCentre.dashboard');
+        }elseif(Auth::user()->user_type == 'bangladeshi-embassy')
+        {
+            //return redirect()->route('OneStopService.dashboard');
+        }elseif(Auth::user()->user_type == 'uae-admin')
+        {
+            return redirect()->route('UAEAdmin.dashboard');
+        }elseif(Auth::user()->user_type == 'master-one-stop-service')
+        {
+            return redirect()->route('OneStopService.dashboard');
+        }elseif(Auth::user()->user_type == 'one-stop-service-agency')
+        {
+            //return redirect()->route('.dashboard');
+        }elseif(Auth::user()->user_type == 'medical-company')
+        {
+            //return redirect()->route('.dashboard');
+        }elseif(Auth::user()->user_type == 'training-company')
+        {
+            //return redirect()->route('.dashboard');
+        }elseif(Auth::user()->user_type == 'travel-company')
+        {
+            //return redirect()->route('.dashboard');
+        }elseif(Auth::user()->user_type == 'biometric-company')
+        {
+            //return redirect()->route('.dashboard');
+        }elseif(Auth::user()->user_type == 'recruiting-agency')
+        {
+            return redirect()->route('RecruitingAgency.dashboard');
+        }elseif(Auth::user()->user_type == 'bangladeshi-admin')
+        {
+            //return redirect()->route('BangladeshAdmin.dashboard');
+        }elseif(Auth::user()->user_type == 'employer')
+        {
+            //return redirect()->route('.dashboard');
+        }else
+        {
+            return redirect()->route('login');
+        }
+    }
 
     /**
      * Create a new controller instance.
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,29 +32,37 @@
                 @csrf
                 <h4 style= color:red;></h4>
                 <div class="input-group mb-3">
-                    <input type="email" class="form-control" name="email" placeholder="Enter your Email" required>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Enter your Email" value="{{ old('email') }}"   autocomplete="email" autofocus>
+
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-envelope"></span>
                         </div>
                     </div>
+
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
-                @error('email')
-                <div class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></div>
-                @enderror
 
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control" name="password" placeholder="Enter your Password" required>
-                    <div class="input-group-append">
+
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Enter your Password"  autocomplete="current-password">
+
+                   <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
                         </div>
                     </div>
-                </div>
 
-                @error('password')
-                <div class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></div>
-                @enderror
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
 
                 <div class="row">
                     <div class="col-12">
