c815262b07a13ab636db6bfe6701e891ea6a9044
Arafat7616 <arafathossin7616@gmail.com>
2021-07-14 15:04:30 +0600

    authentications problem solved

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -32,49 +32,49 @@ class LoginController extends Controller
     public function redirectTo()
     {
         if (Auth::user()->user_type == 'super-admin'){
-            return redirect()->route('SuperAdmin.dashboard');
+            return route('SuperAdmin.dashboard');
         }elseif(Auth::user()->user_type == 'employer-company')
         {
-            return redirect()->route('EmployerCompany.dashboard');
+            return route('EmployerCompany.dashboard');
         }elseif(Auth::user()->user_type == 'welfare-service-center-company')
         {
-            return redirect()->route('WelfareCentre.dashboard');
+            return route('WelfareCentre.dashboard');
         }elseif(Auth::user()->user_type == 'bangladeshi-embassy')
         {
-            return redirect()->route('BangladeshEmbassy.dashboard');
+            return route('BangladeshEmbassy.dashboard');
         }elseif(Auth::user()->user_type == 'uae-admin')
         {
-            return redirect()->route('UAEAdmin.dashboard');
+            return route('UAEAdmin.dashboard');
         }elseif(Auth::user()->user_type == 'master-one-stop-service')
         {
-            return redirect()->route('OneStopService.dashboard');
+            return route('OneStopService.dashboard');
         }elseif(Auth::user()->user_type == 'one-stop-service-agency')
         {
-            //return redirect()->route('.dashboard');
+            //return route('.dashboard');
         }elseif(Auth::user()->user_type == 'medical-company')
         {
-            //return redirect()->route('.dashboard');
+            //return route('.dashboard');
         }elseif(Auth::user()->user_type == 'training-company')
         {
-            //return redirect()->route('.dashboard');
+            //return route('.dashboard');
         }elseif(Auth::user()->user_type == 'travel-company')
         {
-            //return redirect()->route('.dashboard');
+            //return route('.dashboard');
         }elseif(Auth::user()->user_type == 'biometric-company')
         {
-            //return redirect()->route('.dashboard');
+            //return route('.dashboard');
         }elseif(Auth::user()->user_type == 'recruiting-agency')
         {
-            return redirect()->route('RecruitingAgency.dashboard');
+            return route('RecruitingAgency.dashboard');
         }elseif(Auth::user()->user_type == 'bangladeshi-admin')
         {
-            //return redirect()->route('BangladeshAdmin.dashboard');
+            return route('BangladeshAdmin.dashboard');
         }elseif(Auth::user()->user_type == 'employer')
         {
-            //return redirect()->route('.dashboard');
+            //return route('.dashboard');
         }else
         {
-            return redirect()->route('login');
+            return route('login');
         }
     }
 
