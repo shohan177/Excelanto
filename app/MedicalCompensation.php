d94b10438b5a82bb919b1c69b1f2b67af1b5f824
Arafat7616 <arafathossin7616@gmail.com>
2021-08-01 14:46:15 +0600

    model migration for MedicalCompensation

---
+++ app/MedicalCompensation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalCompensation extends Model
+{
+    protected $fillable = [
+        'id',
+        'candidate_id',
+        'company_id',
+        'wsc_id',
+        'candidate_name',
+        'company_name',
+        'sick_type',
+        'doctor_name',
+        'hospital_name',
+        'prescription',
+        'insurance_number',
+        'insurance_card',
+        'comments',
+        'service_status',
+        'created_id',
+        'deleted'
+    ];
+}
