c49b8a5303e2b0bb1033dca6892817a0bdbf1fa2
Arafat7616 <arafathossin7616@gmail.com>
2021-08-10 16:34:26 +0600

    Medical agency done in MOSS

--- app/Http/Controllers/OneStopService/MedicalAgencyController.php
+++ app/Http/Controllers/OneStopService/MedicalAgencyController.php
@@ -3,19 +3,29 @@
 namespace App\Http\Controllers\OneStopService;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 
 class MedicalAgencyController extends Controller
 {
     public function request(){
-        return view('OneStopService.medicalAgency.request');
+        $users = User::where('user_type','medical-agency')
+                     ->where('active_status', 'Pending')
+                     ->orderBy('id','DESC')->get();
+        return view('OneStopService.medicalAgency.request', compact('users'));
     }
 
     public function approved(){
-        return view('OneStopService.medicalAgency.approved');
+        $users = User::where('user_type','medical-agency')
+                     ->where('active_status', 'Approved')
+                     ->orderBy('id','DESC')->get();
+        return view('OneStopService.medicalAgency.approved', compact('users'));
     }
 
     public function rejected(){
-        return view('OneStopService.medicalAgency.rejected');
+        $users = User::where('user_type','medical-agency')
+                     ->where('active_status', 'Rejected')
+                     ->orderBy('id','DESC')->get();
+        return view('OneStopService.medicalAgency.rejected', compact('users'));
     }
 }
--- app/Http/Controllers/OneStopService/TrainingAgencyController.php
+++ app/Http/Controllers/OneStopService/TrainingAgencyController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\OneStopService;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 
 class TrainingAgencyController extends Controller
 {
     public function request(){
-        return view('OneStopService.trainingAgency.request');
+        $users = User::where('user_type','training-agency')
+                     ->where('active_status', 'Pending')
+                     ->orderBy('id','DESC')->get();
+        return view('OneStopService.trainingAgency.request', compact('users'));
     }
 
     public function approved(){
